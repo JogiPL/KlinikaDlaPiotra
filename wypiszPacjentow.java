f0f53845c225f52e50adf35f3cac59ac22eba8a5
JogiPL <krzychu.wasiak@gmail.com>
2017-04-03 22:19:02 +0200

    Delete wypiszPacjentow.java

--- wypiszPacjentow.java
+++
@@ -1,9 +0,0 @@
-
-public class wypiszPacjentow {
-
-    void wypisywanie() {
-        for (int a = 0; a < 23; a++) {
-            System.out.println(pacjenci[a][0] + pacjenci[a][1] + pacjenci[a][2]);  //TUTAJ SIE NIE ZGADZA NA UŻYCIE TABLICY Z INNEGO PLIKU!
-        }
-    }
-}
